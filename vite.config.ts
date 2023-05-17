a9342b6ac313a68c32cf79787e9555f67eb63a1f
tututu <9719002+hutu0903@user.noreply.gitee.com>
2023-05-17 10:17:22 +0800

    change

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,6 @@ import IconsResolver from 'unplugin-icons/resolver';
 // https://vitejs.dev/config/
 export default defineConfig({
 
-    publicPath: '.',
     plugins: [
         vue({
             template: {
